63faadd6bf15b738619e655824f6c123e5857cfb
Phamhieu2119 <phamminhhieu21012003@gmail.com>
2023-09-14 01:02:02 +0700

    thuchanh

---
+++ webThucHanh1/Models/Branch.cs
@@ -0,0 +1,10 @@
+﻿namespace webThucHanh1.Models
+{
+    public enum Branch
+    {
+        IT,
+        BE,
+        CE,
+        EE
+    }
+}
---
+++ webThucHanh1/Models/Gender.cs
@@ -0,0 +1,8 @@
+﻿namespace webThucHanh1.Models
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
